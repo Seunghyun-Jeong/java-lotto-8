5d35a0330630c03cda20d88768369d1671f91544
Seunghyun-Jeong <dkskakdgka@gmail.com>
2025-10-30 14:17:28 +0900

    feat: 로또 발행 기능 구현

--- src/main/java/lotto/LottoGame.java
+++ src/main/java/lotto/LottoGame.java
@@ -1,10 +1,15 @@
 package lotto;
 
 import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
 
 public class LottoGame {
     public void execute() {
         int inputPurchaseMoney = purchase();
+        int purchaseLottoCount = getPurchaseLottoCount(inputPurchaseMoney);
+        List<Lotto> IssueLottoNumbers = getPurchaseLotto(purchaseLottoCount);
     }
 
     private int purchase() {
@@ -21,6 +26,26 @@ public class LottoGame {
                 System.out.println(e.getMessage());
             }
         }
+    }
+
+    private int getPurchaseLottoCount(int inputPurchaseMoney) {
+        int purchaseLottoCount = (inputPurchaseMoney / 1000);
+        System.out.println(purchaseLottoCount + "개를 구매했습니다.");
+        return purchaseLottoCount;
+    }
+
+    private List<Lotto> getPurchaseLotto(int purchaseLottoCount) {
+        List<Lotto> purchaseLotto = new ArrayList<>();
+        for (int i = 0; i < purchaseLottoCount; i++) {
+            List<Integer> lottoNumbers = Randoms.pickUniqueNumbersInRange(1, 45, 6).stream().sorted().toList();
+            purchaseLotto.add(new Lotto(lottoNumbers));
+        }
+
+        for (Lotto lotto : purchaseLotto) {
+            lotto.printLottoNumbers();
+        }
+
+        return purchaseLotto;
     }
 
     private void validatePurchaseMoney(int inputPurchaseMoney) {
