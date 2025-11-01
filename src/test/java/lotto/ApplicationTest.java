89afe76bcb3508edf2ce081cff49a7c0f263153f
Seunghyun-Jeong <dkskakdgka@gmail.com>
2025-11-01 20:00:19 +0900

    test: 구현한 기능 테스트 코드 작성

--- src/test/java/lotto/ApplicationTest.java
+++ src/test/java/lotto/ApplicationTest.java
@@ -54,6 +54,38 @@ class ApplicationTest extends NsTest {
         });
     }
 
+    @Test
+    void 구입금액이_1000원_단위가_아니면_예외발생() {
+        assertSimpleTest(() -> {
+            runException("2500");
+            assertThat(output()).contains(ERROR_MESSAGE);
+        });
+    }
+
+    @Test
+    void 당첨번호에_숫자가_아닌값_입력시_예외발생() {
+        assertSimpleTest(() -> {
+            runException("5000", "1,2,3,4,a,6");
+            assertThat(output()).contains(ERROR_MESSAGE);
+        });
+    }
+
+    @Test
+    void 보너스번호가_당첨번호와_중복되면_예외발생() {
+        assertSimpleTest(() -> {
+            runException("3000", "1,2,3,4,5,6", "6");
+            assertThat(output()).contains(ERROR_MESSAGE);
+        });
+    }
+
+    @Test
+    void 보너스번호가_1미만_또는_45초과면_예외발생() {
+        assertSimpleTest(() -> {
+            runException("3000", "1,2,3,4,5,6", "46");
+            assertThat(output()).contains(ERROR_MESSAGE);
+        });
+    }
+
     @Override
     public void runMain() {
         Application.main(new String[]{});
